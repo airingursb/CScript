65d9ed3897a07412feea444b6ab15395529f6227
Airing <airingdeng@tencent.com>
2023-01-25 14:47:45 +0800

    ci: update tsconfig.json and add some todo comment

---
+++ assets/demo.cs
@@ -0,0 +1,2 @@
+// TODO: type annotation 解析仍然有问题，待定位。
+let myAge:number = 18;
--- assets/hello.cs
+++ assets/hello.cs
@@ -1,16 +1,25 @@
 // 词法解析+语法解析用例
-function hello() {
-  let a:number = 1;
+function hello(input) {
+  // let a = input;
   let b = 2;
-  let c = a + b;
+  let c = b + 1;
   let d = "hello";
   println(d + ":" + c);
   return c;
 }
 
-let a = hello();
+let a = hello(1);
 println("a: " + a);
 
 for (let i = 0; i < 10; i++) {
   println("i:" + i);
 }
+
+// TODO: if 语句有问题，待定位
+/**
+ if (a > 10) {
+  println("a > 10, a:" + a);
+} else {
+  println("a <= 10, a:" + a);
+}
+*/
